1aa1b5cb5cf64312bf36fa089295f88e243fb7b9
Eesha <88975253+EeshaKavuri@users.noreply.github.com>
2022-04-25 23:12:28 +0530

    Update register.php

--- register.php
+++ register.php
@@ -103,7 +103,7 @@ mysqli_close($conn);
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Php Login System</a>
+  <a class="navbar-brand" href="#">Prime Video Clone!</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
