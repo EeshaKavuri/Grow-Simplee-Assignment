688180f3ffd7cc6c2220f1c50c8c8dd345909047
Eesha <88975253+EeshaKavuri@users.noreply.github.com>
2022-04-28 00:00:43 +0530

    Update config.php

--- config.php
+++ config.php
@@ -15,3 +15,7 @@ $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($conn == false){
     dir('Error: Cannot connect');
 }
+else{
+    echo "Connection was successful<br>";
+}
+?>
